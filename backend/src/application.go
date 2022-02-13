894f5005795cc1cbc4824ade4fa1ab9cab66a7aa
morscino <morscino@gmail.com>
2022-02-13 05:50:02 +0100

    initial setup

---
+++ backend/src/application.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"rest":    false,
+			"graphql": true,
+		})
+	})
+
+	r.Run()
+}
